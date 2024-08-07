9df4dc2595e77290a5331d4f63289bbe9c2c0bf7
johnovelli <joh.novelli90@gmail.com>
2024-08-07 11:57:07 -0300

    Alteração nos textos da home e about

--- src/pages/About/texts.js
+++ src/pages/About/texts.js
@@ -2,13 +2,13 @@ const AboutTexts = {
   'pt-br': {
     title: 'Muito prazer! Meu nome é George Novelli Tarlazis.',
     textBlock1: `Antes de fazer a transição para a carreira de desenvolvedor full-stack, construí uma carreira de 8 anos em eventos corporativos, onde iniciei do zero até me tornar técnico de vídeo e streaming de eventos ao vivo. Essa experiência aprimorou minhas habilidades em aprender novas tecnologias, gerenciamento de projetos, resolução de problemas e comunicação eficaz, que são significativas para o desenvolvimento de software.`,
-    textBlock2: `Completei o curso de desenvolvimento full-stack na Trybe, onde adquiri uma sólida base em front-end e back-end. Aprendi a criar e consumir APIs REST, desenvolver interfaces dinâmicas e responsivas e implementar operações CRUD seguras e eficientes. Além disso, adquiri experiência em testes automatizados, o que consolidou minha decisão de seguir carreira na programação.`,
+    textBlock2: `Recentemente completei o curso de desenvolvimento full-stack na Trybe, onde adquiri uma sólida base em front-end e back-end.O que consolidou minha decisão de seguir carreira na programação.`,
   },
   
   'en-us': {
     title: 'Nice to meet you! My name is George Novelli Tarlazis.',
     textBlock1: `My interest in programming began during adolescence, but I ended up graduating in Advertising and built an 8-year career in corporate events, where I went from scratch to becoming a video and live event streaming technician. This experience enhanced my skills in learning new technologies, project management, problem-solving, and effective communication, which are significant for software development.`,
-    textBlock2: `I completed the full-stack development course at Trybe, where I gained a solid foundation in front-end and back-end. I learned to create and consume REST APIs, develop dynamic and responsive interfaces, and implement secure and efficient CRUD operations. Additionally, I gained experience in automated testing, which solidified my decision to pursue a career in programming.`,
+    textBlock2: `I completed the full-stack development course at Trybe, where I gained a solid foundation in front-end and back-end. Which solidified my decision to pursue a career in programming.`,
   },
 };
 
